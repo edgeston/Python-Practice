84721b9e7ae3343b0db4a0e3bf858ebaee3fc52f
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-05-07 16:46:57 -0500

    Add files via upload

---
+++ Greeting.py
@@ -0,0 +1,10 @@
+# Hello World Assignment
+print('Hello world')
+print('What is your name?')  # Ask for name
+myName = input()
+print('It is so good to meet you ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')  # Ask for age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' within a year')
